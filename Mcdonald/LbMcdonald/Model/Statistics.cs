9f0a589ce9e783c8919dce56156a7225f5380e93
rjsdnqkr1 <39177603+rjsdnqkr1@users.noreply.github.com>
2019-09-18 14:57:28 +0900

    [ADD] Base

---
+++ Mcdonald/LbMcdonald/Model/Statistics.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mcdonald.Model
+{
+    class Statistics
+    {
+        public DateTime date { get; set; }
+        public List<Menu> menuList { get; set; }
+
+        public Statistics(DateTime date, List<Menu> menuList)
+        {
+            this.date = date;
+            this.menuList = menuList;
+        }
+    }
+}
---
+++ Mcdonald/LbMcdonald/Model/Table.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mcdonald.Model
+{
+    enum PaymentType { CARD, CASH }
+    class Table
+    {
+        public int idx { get; set; }
+        public List<Menu> menuList { get; set; }
+        public PaymentType payment { get; set; }
+        public int totalPrice { get; set; }
+
+        public Table(int idx, List<Menu> menuList, PaymentType payment, int totalPrice)
+        {
+            this.idx = idx;
+            this.menuList = menuList;
+            this.payment = payment;
+            this.totalPrice = totalPrice;
+        }
+    }
+}
